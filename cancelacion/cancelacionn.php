3392211b73e58bff165621393f1909b924304b69
SebastianMora117 <129330647+SebastianMora117@users.noreply.github.com>
2023-09-17 15:44:11 -0500

    Update cancelacionn.php

--- cancelacion/cancelacionn.php
+++ cancelacion/cancelacionn.php
@@ -1,4 +1,5 @@
 <?php
+
 include('../PHP/Funciones.php');
 
 
@@ -10,66 +11,54 @@ if (isset($_GET['cerrar_sesion'])) {
 }
 
 ?>
-
-
-
-
-
-
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <link rel="stylesheet" href="../cancelacion/cancelacion.css">
     <link rel="stylesheet" href="../cancelacion/notificacion.css">
-    <title>cancelacion de reserva</title>
+    <title>Cancelación de Reserva</title>
 </head>
 <body>
     <header>
         <div class="container">
             <p class="logo">CeniLockers!</p>
             <nav>
-            <a href="../paginainicio/index.php">Inicio</a>
-                <a href="#caracteristicas">Contactenos</a>
+                <a href="../paginainicio/index.php">Inicio</a>
+                <a href="#caracteristicas">Contáctenos</a>
                 <a href="../Soporte/index.php">Soporte</a>
                 <a href="?cerrar_sesion=1">Cerrar Sesión</a>
-
-
-                
-                
             </nav>
         </div>
     </header>
     <section id="hero">
-        <h2>TIEMPO RESTANTE DEL LOKER<br>
-        este es el tiempo que tienes para utilizar el loker</h2><br><br>
+        <h2>TIEMPO RESTANTE DEL LOCKER<br>Este es el tiempo que tienes para utilizar el locker</h2><br><br>
         <div class="container">
-          <p id="tiempo">00:00:00</p>
+            <p id="tiempo">00:00:00</p>
         </div>
     </section>
     <br><br><br>   <br>
     <section id="hero1" >
-       
         <form action="../cancelacion/cancelacion.php" method="post" class="contenedor" >
-            <h2>CANCELACION</h2>
-                <input type="text" name="porque" placeholder="razon de la cancelacion" ><br>
-                <input type="number" name="casillero" placeholder="casillero cual se desea cancelar la reserva" ><br>
-                <input type="text" name="nom" placeholder="nombre del estudiante"><br>
-                <input type="submit" id="cancelar"  value="cancelar reserva">
+            <h2>CANCELACIÓN</h2>
+            <input type="text" name="porque" placeholder="Razón de la cancelación" ><br>
+            <input type="number" name="casillero" placeholder="Casillero que se desea cancelar la reserva" ><br>
+            <input type="text" name="nom" placeholder="Nombre del estudiante"><br>
+            <input type="button" id="cancelar" value="Cancelar Reserva">
         </form>
     </section>
     <br><br><br><br>
     <footer>
         <div class="container">
             <p>
-                    Linea gratuita 018000000-0000<br><br>
-                    Correo:LockersCenigraf@soy.sena.edu.co <br><br>
-                    Direccion:Cl. 15 #31-42, Bogotá<br><br>
-                    Servicio al cliente: 311 295 77 08<br><br>
-                    &copy; CeniLockers!
+                Línea gratuita 018000000-0000<br><br>
+                Correo: LockersCenigraf@soy.sena.edu.co <br><br>
+                Dirección: Cl. 15 #31-42, Bogotá<br><br>
+                Servicio al cliente: 311 295 77 08<br><br>
+                &copy; CeniLockers!
             </p>
         </div>
     </footer>
     <script src="../cancelacion/notificacion.js"></script>
+    
 </body>
 </html>
